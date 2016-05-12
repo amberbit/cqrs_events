d62285311a6a3421aeee1b4553887b17e9c59719
Hubert Łępicki <hubert.lepicki@amberbit.com>
2016-05-12 14:35:28 +0200

    initial scaffold

---
+++ README.md
@@ -0,0 +1,20 @@
+# Cqrs.Events
+
+**TODO: Add description**
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed as:
+
+  1. Add cqrs_events to your list of dependencies in `mix.exs`:
+
+        def deps do
+          [{:cqrs_events, "~> 0.0.1"}]
+        end
+
+  2. Ensure cqrs_events is started before your application:
+
+        def application do
+          [applications: [:cqrs_events]]
+        end
+
---
+++ mix.exs
@@ -0,0 +1,35 @@
+defmodule Cqrs.Events.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :cqrs_events,
+     version: "0.0.1",
+     elixir: "~> 1.2",
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     deps: deps]
+  end
+
+  # Configuration for the OTP application
+  #
+  # Type "mix help compile.app" for more information
+  def application do
+    [applications: [:logger, :phoenix], # replace phoenix with phoenix_pubsub when released
+     mod: {Cqrs.Events, []}]
+  end
+
+  # Dependencies can be Hex packages:
+  #
+  #   {:mydep, "~> 0.3.0"}
+  #
+  # Or git/path repositories:
+  #
+  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
+  #
+  # Type "mix help deps" for more examples and options
+  defp deps do
+    [
+      {:phoenix, "~> 1.1.4"}, # replace with phoenix_pubsub once it's released
+    ]
+  end
+end
